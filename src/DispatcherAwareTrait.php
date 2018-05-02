2275bbed23d63cb68d91d01a0d4dd96f3a603a2d
Michael Bardsley <mic.bardsley@outlook.com>
2018-05-02 22:10:36 +0100

    Added new methods to the trait to improve its functionality

--- src/DispatcherAwareTrait.php
+++ src/DispatcherAwareTrait.php
@@ -2,6 +2,15 @@
 
 namespace Mic2100\Events;
 
+/**
+ * Trait DispatcherAwareTrait
+ *
+ * @category Events
+ * @package  Mic2100\Events
+ * @author   Michael Bardsley @mic_bardsley
+ * @link     http://github.com/mic2100/events
+ * @licence  MIT
+ */
 trait DispatcherAwareTrait
 {
     /**
@@ -10,6 +19,8 @@ trait DispatcherAwareTrait
     private $dispatcher;
 
     /**
+     * Gets the dispatcher
+     *
      * @return Dispatcher
      */
     public function getDispatcher() : Dispatcher
@@ -18,10 +29,57 @@ trait DispatcherAwareTrait
     }
 
     /**
+     * Sets the dispatcher
+     *
      * @param Dispatcher $dispatcher
      */
     public function setDispatcher(Dispatcher $dispatcher)
     {
         $this->dispatcher = $dispatcher;
+    }
+
+    /**
+     * Triggers the event(s) given the handle
+     *
+     * @param string $handle
+     * @param array|null $params
+     *
+     * @throws \Exception
+     */
+    public function dispatcherTriggerEvent(string $handle, array $params = null)
+    {
+        $this->getDispatcher()->trigger($handle, $params);
+    }
+
+    /**
+     * Removes the event(s) from the dispatcher
+     *
+     * @param string $handle
+     */
+    public function dispatcherRemoveEvent(string $handle)
+    {
+        $this->getDispatcher()->removeEvent($handle);
+    }
+
+    /**
+     * Adds an event to the dispatcher
+     *
+     * @param EventInterface $event
+     * @return bool
+     */
+    public function dispatcherAddEvent(EventInterface $event) : bool
+    {
+        $this->getDispatcher()->addEvent($event);
+    }
+
+    /**
+     * Has the dispatcher got the event?
+     *
+     * @param string $handle
+     * @return bool
+     */
+    public function dispatcherHasEvent(string $handle) : bool
+    {
+        return $this->getDispatcher()->hasEvent($handle);
     }
 }
